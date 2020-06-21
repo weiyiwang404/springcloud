40dddc4dd866d182c9a8e139e34785ba5f627827
wangwy <weiyiwang404@163.com>
2020-06-21 15:32:55 +0800

    gateway网关配置并实现动态路由。

---
+++ springcloud2020/spring-cloud-gatway9527/src/main/java/com/weiyi/springcloud/GatewayMain9527.java
@@ -0,0 +1,19 @@
+package com.weiyi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * description:
+ *
+ * @author weiyi
+ * @date 2020/6/21 14:29
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
