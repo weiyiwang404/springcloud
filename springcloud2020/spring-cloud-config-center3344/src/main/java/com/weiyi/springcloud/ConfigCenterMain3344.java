5d086cffd686c4086ff1d0d775dc879d67fbf97b
wangwy <weiyiwang404@163.com>
2020-06-24 23:31:19 +0800

    configcenter服务配置。

---
+++ springcloud2020/spring-cloud-config-center3344/src/main/java/com/weiyi/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,21 @@
+package com.weiyi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * description:
+ *
+ * @author weiyi
+ * @date 2020/6/24 23:12
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
